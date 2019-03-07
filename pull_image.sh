549c5fef9625625c729280886e16a1b9b75a1070
Cory <caw5cv@virginia.edu>
2019-03-07 16:57:11 -0500

    various fixes
    fixed command substitution errors in slurm script; fixed the prune_GAF file to correctly print; added script to pull the iMapSplice image file from singularity hub

---
+++ pull_image.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+module load singularity
+singularity pull -n iMapSplice.simg shub://cory-weller/iMapSplice.simg
--- scripts/prune_GAF.sh
+++ scripts/prune_GAF.sh
@@ -6,7 +6,7 @@ desired_chromosome_ids=( "chr2L" "chr2R" "chr3L" "chr3R" "chrX" )
 raw_gene_annotation_file="${1}"
 pruned_gene_annotation_file="${1%.gaf}.pruned.gaf"
 
-
 for chromosome in ${desired_chromosome_ids[@]}; do
-    grep -E "\${chromosome}\s[\+\-]\s[0-9]+\s" ${raw_gene_annotation_file} >> ${pruned_gene_annotation_file}
+    pattern="^.*\s${chromosome}\s[\+\-]\s[0-9]+\s"
+    grep -E $pattern ${raw_gene_annotation_file}
 done
